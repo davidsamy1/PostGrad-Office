2e4e4283974c5db38d8609a1f6dce93a2638c82d
Mahmoud Hosam <mahmoudhosam44@gmail.com>
2022-02-07 10:41:38 +0200

    remmoved useless code from navigation.js

--- DatabaseWebsite/DatabaseWebsite/scripts/navigation.js
+++ DatabaseWebsite/DatabaseWebsite/scripts/navigation.js
@@ -3,12 +3,9 @@ var menu = document.getElementById("menu");
 var content = document.querySelector(".main-content");
 var navItems = document.querySelector(".nav-items");
 var navLinks = document.querySelectorAll(".nav-link");
-var screenWidth = window.outerWidth;
 menu.addEventListener("click", () => {
   if (nav.style.width == "85px") {
     nav.style.width = "300px";
-    nav.style.minHeight = "600px";
-    nav.style.height = "90vh";
     content.style.marginLeft = "3rem";
     navItems.style.display = "block";
     for (i = 0; i < navLinks.length; i++) {
@@ -16,19 +13,9 @@ menu.addEventListener("click", () => {
     }
   } else {
     nav.style.width = "85px";
-    nav.style.height = "90vh";
-    content.style.marginLeft = "-180px";
+    content.style.marginLeft = "-11.25rem";
     for (i = 0; i < navLinks.length; i++) {
       navLinks[i].style.display = "none";
     }
   }
 });
-
-if (screenWidth <= 768) {
-  nav.style.width = "85px";
-  nav.style.height = "90vh";
-  content.style.marginLeft = "-180px";
-  for (i = 0; i < navLinks.length; i++) {
-    navLinks[i].style.display = "none";
-  }
-}
