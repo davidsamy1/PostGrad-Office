cfdd0c12da4ec6be80c241c172e3ce88e5acafeb
Mahmoud Hosam <mahmoudhosam44@gmail.com>
2022-02-10 10:51:12 +0200

    Added Paging function to handle paging events

--- DatabaseWebsite/DatabaseWebsite/search.aspx.cs
+++ DatabaseWebsite/DatabaseWebsite/search.aspx.cs
@@ -14,30 +14,25 @@ namespace DatabaseWebsite
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
         }
 
         protected void Button1_Click(object sender, EventArgs e)
         {
             String connStr = WebConfigurationManager.ConnectionStrings["DatabaseWebsite"].ToString();
-
             SqlConnection conn = new SqlConnection(connStr);
-
             String Thesis = thesis.Text;
 
+            conn.Open();
             SqlCommand loginProc = new SqlCommand("searchThesis", conn);
             loginProc.CommandType = CommandType.StoredProcedure;
 
             loginProc.Parameters.Add(new SqlParameter("@name", SqlDbType.VarChar)).Value = Thesis;
-
-
-            conn.Open();
-            
             loginProc.ExecuteNonQuery();
-            SqlDataAdapter adapter = new SqlDataAdapter(loginProc);
-            DataTable da = new DataTable();
-            adapter.Fill(da);
-            GridView1.DataSource = da;
+
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter(loginProc);
+            da.Fill(ds);
+            GridView1.DataSource = ds;
             GridView1.DataBind();
             conn.Close();
             if (GridView1.Rows.Count ==0)
@@ -47,6 +42,28 @@ namespace DatabaseWebsite
                 label.CssClass = "fail";
                 form1.Controls.Add(label);
             }
+        }
+         
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e) {  
+            GridView1.PageIndex = e.NewPageIndex;
+            String connStr = WebConfigurationManager.ConnectionStrings["DatabaseWebsite"].ToString();
+            SqlConnection conn = new SqlConnection(connStr);
+            String Thesis = thesis.Text;
+
+            conn.Open();
+            SqlCommand loginProc = new SqlCommand("searchThesis", conn);
+            loginProc.CommandType = CommandType.StoredProcedure;
+
+            loginProc.Parameters.Add(new SqlParameter("@name", SqlDbType.VarChar)).Value = Thesis;
+            loginProc.ExecuteNonQuery();
+
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter(loginProc);
+            da.Fill(ds);
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+            conn.Close();
+
         }
 
         protected void back(object sender, EventArgs e)
