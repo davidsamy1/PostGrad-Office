60179abf847f5553baa92a633169c5254cc39197
Mahmoud Hosam <mahmoudhosam44@gmail.com>
2022-02-10 11:16:31 +0200

    added the pager fuctionality

--- DatabaseWebsite/DatabaseWebsite/defenses.aspx.cs
+++ DatabaseWebsite/DatabaseWebsite/defenses.aspx.cs
@@ -39,6 +39,31 @@ namespace DatabaseWebsite
             conn.Close();
         }
 
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e) {  
+            GridView1.PageIndex = e.NewPageIndex;
+            String connStr = WebConfigurationManager.ConnectionStrings["DatabaseWebsite"].ToString();
+
+            SqlConnection conn = new SqlConnection(connStr);
+
+
+            SqlCommand view = new SqlCommand("viewDefenses", conn);
+            view.CommandType = CommandType.StoredProcedure;
+            //int id = int.Parse(input.Text);
+            view.Parameters.Add(new SqlParameter("@id", SqlDbType.Int)).Value = Session["user"];
+
+
+
+
+            conn.Open();
+                view.ExecuteNonQuery();
+                SqlDataAdapter adapter = new SqlDataAdapter(view);
+                DataTable da = new DataTable();
+                adapter.Fill(da);
+                GridView1.DataSource = da;
+                GridView1.DataBind();
+            conn.Close();
+        }
+
         protected void back(object sender, EventArgs e)
         {
             Response.Redirect("examiner.aspx");
@@ -47,26 +72,4 @@ namespace DatabaseWebsite
     }
           
 }
-            /*SqlDataReader reader = view.ExecuteReader();
-
-            if (reader.HasRows)
-            {
-
-                String s = "";
-                Label label1 = new Label();
-                label1.Text = "Title | Student | Supervisor" + "<br/>";
-                form1.Controls.Add(label1);
-                while (reader.Read())
-                {
-                    Label label = new Label();
-                    s = String.Format("{0} | \t{1} | \t{2}", reader.GetString(0),
-                        reader.GetString(1), reader.GetString(2));
-                    label.Text = s + "<br/>";
-                    form1.Controls.Add(label);
-                }
-            }
-            else
-            {
-                Console.WriteLine("No rows found.");
-            }
-            reader.Close();*/
+ 
