b66130f10ad7c9275a64db2a55cdf0b87caa12b3
Mahmoud Hosam <mahmoudhosam44@gmail.com>
2022-02-17 16:41:04 +0200

    create a new webform for supervisor list

---
+++ DatabaseWebsite/DatabaseWebsite/supervisors.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DatabaseWebsite
+{
+    public partial class supervisors : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string connStr = WebConfigurationManager.ConnectionStrings["DatabaseWebsite"].ToString();
+            //create a new connection
+            SqlConnection conn = new SqlConnection(connStr);
+
+            SqlCommand ListSup = new SqlCommand("AdminListSup", conn);
+            ListSup.CommandType = CommandType.StoredProcedure;
+
+            conn.Open();
+                ListSup.ExecuteNonQuery();
+                SqlDataAdapter adapter = new SqlDataAdapter(ListSup);
+                DataTable da = new DataTable();
+                adapter.Fill(da);
+                GridView1.DataSource = da;
+                GridView1.DataBind();
+            conn.Close();
+        }
+    }
+}
